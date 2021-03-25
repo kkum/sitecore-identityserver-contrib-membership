fb3c515b9b910b353aee8e13409b60d6af40ddd7
Alexander Naumchenkov <alexander.naumchenkov@sitecore.com>
2021-03-25 15:03:47 +0200

    Release/6.0.0 (#4)
    * Migrate to latest IS4
    
    Signed-off-by: SalifovEA <sall.if.of@gmail.com>
    
    * Remove hardcoded connection string
    
    Signed-off-by: SalifovEA <sall.if.of@gmail.com>
    
    * Update version, Log level and readme
    
    Signed-off-by: SalifovEA <sall.if.of@gmail.com>
    
    * Downgrade DependencyInjection.Abstractions reference version
    
    Signed-off-by: SalifovEA <sall.if.of@gmail.com>
    
    * Update readme.md
    
    * Update .appveyor.yml
    
    * Update cake. Fix breaking changes.
    
    * Adjust to use VS2019.
    
    * Update owner, repo name, package properties.
    
    * Fix repo url.
    
    * Fix misprint.
    
    * Update readme.md
    
    * Update IdentityServer4.Contrib.Membership.csproj
    
    Co-authored-by: wwwlicious <773dcf03@opayq.com>
    Co-authored-by: SalifovEA <sall.if.of@gmail.com>

--- src/samples/IdentityServer4.Contrib.Membership.ClientDemo/Program.cs
+++ src/samples/IdentityServer4.Contrib.Membership.ClientDemo/Program.cs
@@ -1,22 +1,23 @@
-﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 namespace IdentityServer4.Contrib.Membership.ClientDemo
 {
-    using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
-    
+    using Microsoft.Extensions.Hosting;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
-        
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- src/samples/IdentityServer4.Contrib.Membership.ClientDemo/Startup.cs
+++ src/samples/IdentityServer4.Contrib.Membership.ClientDemo/Startup.cs
@@ -1,23 +1,29 @@
-﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 namespace IdentityServer4.Contrib.Membership.ClientDemo
 {
-    using System.IdentityModel.Tokens.Jwt;
-    using Microsoft.AspNetCore.Authentication.Cookies;
-    using Microsoft.AspNetCore.Authentication.OpenIdConnect;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
 
             services.AddAuthentication(options =>
                 {
@@ -30,8 +36,8 @@ namespace IdentityServer4.Contrib.Membership.ClientDemo
                     options.SignInScheme = "Cookies";
 
                     // Authority must be handled via https only and RequireHttpsMetadata must be set to true on production
-                    options.RequireHttpsMetadata = false;
-                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = true;
+                    options.Authority = "https://localhost:44323";
                     options.ClientId = "ServiceStack.SelfHost";
                     options.ClientSecret = "F621F470-9731-4A25-80EF-67A6F7C5F4B8";
                     options.ResponseType = "code id_token";
@@ -46,15 +52,24 @@ namespace IdentityServer4.Contrib.Membership.ClientDemo
                 });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseRouting();
+
             app.UseAuthentication();
+            app.UseAuthorization();
 
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-
-            app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
